ac17b36fb653a14a75d2b7a4731597a3e23e37f0
Philippe Dixon <pdixon555@hotmail.com>
2017-09-26 17:00:34 -0400

    finished code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friendsArray = [{
     "name": "Sarah",
-    "photo": "https://i.pinimg.com/736x/50/1f/fa/501ffaae9b1a0477f75898926fe0708c--disney-princess-disney-cruiseplan.jpg",
+    "photo": "http://weknowyourdreams.com/images/woman/woman-02.jpg",
     "scores[]": ["1", "1", "2", "4", "3", "1", "4", "3", "4", "5"]
 },{
     "name": "Hailey",
-    "photo": "https://i.pinimg.com/736x/50/1f/fa/501ffaae9b1a0477f75898926fe0708c--disney-princess-disney-cruiseplan.jpg",
+    "photo": "https://i.pinimg.com/736x/91/f1/f6/91f1f684d8eee8afe0cfd8b53aaf842e--girls-selfies-beautiful-eyes.jpg",
     "scores[]": ["3", "5", "4", "3", "4", "5", "5", "4", "2", "4"]
 },{
     "name": "Michelle",
-    "photo": "https://i.pinimg.com/736x/50/1f/fa/501ffaae9b1a0477f75898926fe0708c--disney-princess-disney-cruiseplan.jpg",
+    "photo": "https://www.thesun.co.uk/wp-content/uploads/2017/03/nintchdbpict000307208318.jpg?strip=all&w=484",
     "scores[]": ["2", "4", "3", "2", "4",  "5", "4", "5", "3", "1"]
 },{
     "name": "Jessica",
-    "photo": "https://i.pinimg.com/736x/50/1f/fa/501ffaae9b1a0477f75898926fe0708c--disney-princess-disney-cruiseplan.jpg",
+    "photo": "https://resizing.flixster.com/QeM-9t-G2f3V2gRBhw-OSOp17Og=/633x704/v1.cjs0Mjc1NjtwOzE3NDYwOzEyMDA7NjMzOzcwNA",
     "scores[]": ["2", "4", "5", "4",  "5", "4", "3", "4", "5", "2"]
 },{
     "name": "Marie",
-    "photo": "https://i.pinimg.com/736x/50/1f/fa/501ffaae9b1a0477f75898926fe0708c--disney-princess-disney-cruiseplan.jpg",
+    "photo": "https://www.esafety.gov.au/-/media/cesc/esafetywomen/esafetywomen-290x290-woman-on-mobile-phone.jpg?h=290&la=en&w=290&c=true&ch=290&cw=290&hash=F1ECFFF84E57597F1785105888236802FE255848",
     "scores[]": ["1", "4", "2", "1", "3", "5", "2", "3", "4", "5"]
 }]
 
